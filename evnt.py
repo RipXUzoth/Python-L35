4b84f596f45d311bbc69fd06596f14a5ad6ba1fa
RipXUzoth <akkuharish1@gmail.com>
2025-07-27 11:45:23 +0530

    evnt

---
+++ evnt.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+window = tk.Tk()
+window.title("Event handler")
+window.geometry("100x100")
+def handle_keypress(event):
+    """Print the character associated to the key pressed"""
+    print(event.char)
+window.bind("<key>", handle_keypress)
+def handle_click(event):
+    print("\nThe button was clicked!")
+button = tk.Button(text="Click me!")
+button.pack()
+button.bind("<Button-1>", handle_click)
+window.mainloop()
